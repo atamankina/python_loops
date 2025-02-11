378547173044d805390ab88194a0ce38799f3ff4
Galina Atamankina <g.atamankina@gmail.com>
2025-02-11 10:49:14 +0100

    Adding another solution for pizza pieces evaluation.

--- pizza_price_calc_new.py
+++ pizza_price_calc_new.py
@@ -2,6 +2,7 @@ pizza_price = float(input("Wie viel kostet eine Pizza? "))
 number_pieces = int(input("Wie viele Stuecke von Pizza wurden gegessen? "))
 
 pieces_eaten = {}
+pieces_eaten_total = 0
 
 while True:
     name = input("Was ist der Name der Person? (schreibe 'fertig', um die Eingabe zu beenden) ")
@@ -11,13 +12,20 @@ while True:
 
     pieces_per_person = int(input("Wie viele Stuecke hat diese Person gegessen? "))
     pieces_eaten[name] = pieces_per_person
+    pieces_eaten_total += pieces_per_person
 
+print("Sie haben folgende Daten eingetragen:")
 print(f"Pizzapreis: {pizza_price}")
 print(f"Anzahl Stuecke: {number_pieces}")
 print(f"Dictionary: {pieces_eaten}")
 
+if pieces_eaten_total > number_pieces:
+    print(f"Fehler, Anzahl gegessenen Stuecke {pieces_eaten_total} groesser als gesamte Stuecke {number_pieces}.")
+    exit()
+
 price_per_piece = pizza_price / number_pieces
 
+print("Die Rechnungen sind wie folgt:")
 for person in pieces_eaten:
-    bill = price_per_piece * pieces_eaten[person]
+    bill = round(price_per_piece * pieces_eaten[person], 2)
     print(f"{person} hat {pieces_eaten[person]} Stueck Pizza gegessen. Zu zahlen sind {bill}.")
