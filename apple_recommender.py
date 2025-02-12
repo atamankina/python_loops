5eae8ff346eb38202519a92d70eced462980e3bb
Galina Atamankina <g.atamankina@gmail.com>
2025-02-12 11:57:53 +0100

    Using additional conditions and list comprehension with apple recommender.

--- apple_recommender.py
+++ apple_recommender.py
@@ -7,14 +7,19 @@ apple_varieties = {
 
 # Get user preferences
 print("What apple characteristics do you prefer?")
-color_preference = input("What color do you prefer? yellow / green / red : ")
-taste_preference = input("What taste do you prefer? sweet / sour : ")
+color_preference = input("What color do you prefer? yellow / green / red / leave empty for any: ").lower()
+taste_preference = input("What taste do you prefer? sweet / sour / leave empty for any: ").lower()
 
 print(f"Your preferences are: color {color_preference}, taste {taste_preference}.")
 
-matching_apples = []
+matching_apples = [apple for apple, properties in apple_varieties.items() \
+                   if (color_preference == properties["color"] or color_preference == "")and \
+                    (taste_preference == properties["taste"] or taste_preference == "")]
 
-for apple, properties in apple_varieties.items():
-    print(apple)
-    print(properties)
+# for apple, properties in apple_varieties.items():
+#     if (color_preference == properties["color"] or color_preference == "") and \
+#         (taste_preference == properties["taste"] or taste_preference == ""):
+#         matching_apples.append(apple)
 
+print("You should buy these apples: ")
+print(matching_apples)
